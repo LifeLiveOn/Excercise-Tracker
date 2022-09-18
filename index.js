edc363c127e042fe528ab399320a032cd5dac884
Thanh <tnkchaseme@gmail.com>
2022-09-18 18:03:23 -0500

    add user

--- index.js
+++ index.js
@@ -2,13 +2,51 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const bodyParser = require('body-parser')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+// where your node app starts
+
+mongoose.connect(process.env.MONGO_URI3, { useNewUrlParser: true, useUnifiedTopology: true });
+
+var mongoose = require('mongoose')
+//define schema configuration
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  Username:{type:String,required:true},
+})
+
+const ExcerciseSchema = new Schema({
+  userId :{type:String,required:true},
+  description:{type:String},
+  duration:Number,
+  date: Date,
+})
+
+const User = mongoose.model("User",userSchema)
+const Excercise = mongoose.model("Excercise",ExcerciseSchema);
+
+app.post('/api/users', function(req, res){
+  const newUser = new User({
+     username: req.body.username
+  })
+  newUser.save(function(err, data){
+    if(err||!data){
+      res.send("There was an unexpected error when creating user")
+    }
+    else{
+      res.json(data);
+    }
+  })
+})
 
 
 
