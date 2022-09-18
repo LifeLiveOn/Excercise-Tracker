e670c7efcb6a06c4fd09ccdd9cfcaf29b4a38c88
Thanh <tnkchaseme@gmail.com>
2022-09-18 18:29:04 -0500

    add

--- index.js
+++ index.js
@@ -35,7 +35,6 @@ const User = mongoose.model("User",userSchema)
 const Excercise = mongoose.model("Excercise",ExcerciseSchema);
 
 app.post('/api/users', function(req, res){
-  console.log(req.body)
   const newUser = new User({
      username: req.body.username
   })
@@ -50,7 +49,36 @@ app.post('/api/users', function(req, res){
 })
 
 
+app.post('/api/users/:id', function(req, res){
+  const id=req.params.id
+  const {description,duration,date} = req.body
+  User.findById(id,function(err,userData){
+    if(err){
+      res.send("cant find user")
+    }else{
+      const newExercise = new Exercise({
+        userId: id,
+        description: description,
+        duration: duration,
+        date: new Date(date),
+      })
+      newExercise.save(function(err, data){
+        if(err||!data){res.send("cant save")}else{res.json(
+          {
+            username: userData.username,
+            description,
+            duration,
+            date: date.toDateString(),
+            _id:userData._id
+          }
+          
 
+        )}
+      })
+    }
+
+  })
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
