6e2028b5192ece8e1d14e560180e03750e6dd875
thanh <86644469+LifeLiveOn@users.noreply.github.com>
2022-09-29 09:56:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ if(dateFrom==null&&dateTo==null&&limitData==null){
 }
 else{
     try{
-        user = Excercise.find({ date: { $gte: new Date(req.query.from), $lte: new Date(req.query.to)} })
+        user = Excercise.find({ date: { $gte: new Date(req.query.from), $lte: new Date(req.query.to)} }).sort({ date: 'asc'})  
           .then(userLogs => {
 //                 var count = userLogs.length
 //                 let username = null
